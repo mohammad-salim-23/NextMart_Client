b8c1388ade25e1751a64e399a3f7afb626bffd97
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-04-02 00:12:16 +0600

    HeroSection made

---
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { HeroSection } from "@/components/modules/home/HeroSection";
+
+
+const HomePage = ()=>{
+  
+    return (
+        <div>
+            <HeroSection/>
+            
+        </div>
+    )
+}
+export default HomePage;
