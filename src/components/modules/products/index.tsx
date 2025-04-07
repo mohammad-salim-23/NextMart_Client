b3d3fc4b8b3a0f87d7cc8e06c09184446caa3b55
Mohammad Salim <mohammadsalim017427@gmail.com>
2025-04-08 02:18:33 +0600

    create all products banner and category section

---
+++ src/components/modules/products/index.tsx
@@ -0,0 +1,10 @@
+
+const AllProducts = ({products} : any) =>{
+    return (
+        <div className="flex gap-2">
+   <div>Sidebar</div>
+   <div>all Products</div>
+        </div>
+    )
+}
+export default AllProducts;
